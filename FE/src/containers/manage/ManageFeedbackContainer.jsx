f7d22b01a675ae440d4aa25d1d17fdbd941094b9
kimuijin <whitekuj02@naver.com>
2023-02-17 18:24:54 +0900

    임시 커밋 1

---
+++ FE/src/containers/manage/ManageFeedbackContainer.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./ManageFeedbackContainer.module.css";
+
+function ManageFeedbackContainer(){
+    const [작품, 작품변경] = useState([1,2,3]);
+
+    const FeedBackPage = () =>{
+        return(
+            <>
+
+            </>
+        )
+    }
+    return(
+        <div>
+            <h4 style={{textAlign:"left"}}>작품 별 코멘트 목록</h4>
+            {
+                작품.map((data, i)=>{
+
+                })
+            }
+        </div>
+    )
+}
+
+export default ManageFeedbackContainer;
