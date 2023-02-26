bead364e221482d1ecdf5f55e6e7ab0196bd8e26
Joo Hyeon Jung <twincornjr@gmail.com>
2023-02-26 23:58:31 +0900

    수정사항 반영

--- FE/src/layout/Header/Header.jsx
+++ FE/src/layout/Header/Header.jsx
@@ -5,6 +5,7 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import { useNavigate } from "react-router-dom";
 import img from "../../images/로고대체.jpg";
 import axios from "axios";
+import { useState, useEffect } from "react";
 const Style = {
   Header: styled.div`
     position: fixed;
