e466881df054ad4b6228933f9f87fc7c13046c01
kimuijin <whitekuj02@naver.com>
2023-03-07 13:11:17 +0900

    proxy server

--- FE/src/containers/manage/ManageShareContainer.jsx
+++ FE/src/containers/manage/ManageShareContainer.jsx
@@ -159,7 +159,7 @@ function ManageShareContainer() {
     //formdata.append("uploadFiles", uploads); // 이거 오류남 왜징
 
     console.log(formdata)
-    axios.post("http://localhost:8080/reference",formdata,{
+    axios.post("/api/reference",formdata,{
       headers: {"Content-Type": "multipart/form-data"},
       withCredentials : true
     })
