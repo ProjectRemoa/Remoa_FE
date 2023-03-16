5172adc54513d37b442580ebf09f7124cde0d36e
kmyobin <sowhat0516@gmail.com>
2023-03-17 01:42:54 +0900

    내 작업물 목록 GUI 수정

--- FE/src/containers/manage/ManageListContainer.jsx
+++ FE/src/containers/manage/ManageListContainer.jsx
@@ -1,22 +1,63 @@
 import { React, useEffect, useState } from "react";
 import axios from "axios";
 import ManageList from "./ManageList";
+import "./ManageListContainer.scss";
+import { useNavigate } from "react-router";
+
 function ManageListContainer() {
   const [mywork, setMywork] = useState([]);
+  const [totalOfAllReferences, setTotalOfAllReferences] = useState(0);
+  const [totalOfPageElements, setTotalOfPageElements] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const navigate = useNavigate();
+
+  const onChangeCategory = (e) => {
+    navigate(`/manage/list/${e.target.value}`);
+  };
+
   useEffect(() => {
     axios
       .get("/BE/user/reference")
       .then((res) => {
         console.log(res);
-        setMywork([...res.data.data]);
+        setMywork([...res.data.data.references]);
+        setTotalOfAllReferences(res.data.data.totalOfAllReferences);
+        setTotalOfPageElements(res.data.data.totalOfPageElements);
+        setTotalPages(res.data.data.totalPages);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  console.log(mywork);
-  return <ManageList data={mywork} />;
+  return (
+    <div className="ManageListContainer">
+      <div style={{ float: "left" }}>내 작업물 목록</div>
+      <div>
+        <div className="form_radio_btn" style={{ float: "left" }}>
+          <input
+            id="radio-1"
+            type="radio"
+            name="category"
+            value="total"
+            onChange={onChangeCategory}
+          />
+          <label htmlFor="radio-1">전체</label>
+        </div>
+      </div>
+      {!totalOfAllReferences ? (
+        <div>지금 바로 작업물을 올려보세요!</div>
+      ) : (
+        <ManageList
+          data={mywork}
+          TAR={totalOfAllReferences}
+          TPE={totalOfPageElements}
+          TP={totalPages}
+        />
+      )}
+    </div>
+  );
 }
 
 export default ManageListContainer;
