880a2d3d07e491539fb1a61c8a0ab39a11cf2aba
Lim_JiSeon <dpf226@naver.com>
2023-03-19 17:51:26 +0900

    마이페이지 오류 해결

--- FE/src/containers/manage/ManageFeedbackContainer.jsx
+++ FE/src/containers/manage/ManageFeedbackContainer.jsx
@@ -80,7 +80,7 @@ function ManageFeedbackContainer(){
             <div key ={i} style={{display:"flex", margin: "2%", top:`${(i+1) * topValue}%`, width: "100%", height:"25%"}}>
                 
                 <div style={{display:"flex", flexDirection: "column", width: "30%", maxHeight:"100%"}}>
-                    <img src={imgCommentPagTest} style={{filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))", margin: "2%", maxWidth: "100%", maxHeight: "80%"}}/>
+                    <img src={data.thumbnail} style={{filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))", margin: "2%", maxWidth: "100%", maxHeight: "80%"}}/>
                     <button style={{background: "#FADASE", border: "1px solid #B0B0B0", borderRadius: "10px", margin: "1%", marginLeft: "2%", width: "95%", height: "10%"}}
                     onClick = {()=>{
                         Navigete("/")
