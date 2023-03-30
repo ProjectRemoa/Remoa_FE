707f511f54f5650a79e9f7f9cc6f35dad53349ae
kmyobin <sowhat0516@gmail.com>
2023-03-30 11:20:44 +0900

    푸터 수정

--- FE/src/layout/Layout.jsx
+++ FE/src/layout/Layout.jsx
@@ -7,13 +7,11 @@ import styles from "./Layout.module.css";
 function Layout(props) {
   return (
     <>
-    <div className={styles.layout}>
-      <Header />
-      <main className={styles.main}>
-        {props.children}    
-      </main>
-    </div>
-    <Footer />
+      <div className={styles.layout}>
+        <Header />
+        <main className={styles.main}>{props.children}</main>
+        <Footer style={{ flex: "1" }} />{" "}
+      </div>
     </>
   );
 }
