336852269abfb2b434466a14e2fcbcb164f26fc6
Joo Hyeon Jung <twincornjr@gmail.com>
2023-04-01 10:12:34 +0900

    왜 안바뀜

--- FE/src/containers/modal/DetailedFeedback/DetailedFeedbackComment.jsx
+++ FE/src/containers/modal/DetailedFeedback/DetailedFeedbackComment.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState} from "react";
 import { DF } from "../../../layout/DetailFeedbackStyle";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import DetailedFeedbackCommentAgain from "./DetailedFeedbackCommentAgain";
@@ -15,7 +15,7 @@ export default function DetailedFeedbackComment({
   console.log(feedbacks);
 
   const [contents, setContents] = useState("");
-  const [putMemberId, setPutMemberId] = useState(0); //수정할 member id
+  const [putMemberId, setPutMemberId] = useState(0); // 수정할 member id
   const onChangeContents = (event) => {
     setContents(event.target.value);
   };
@@ -38,27 +38,17 @@ export default function DetailedFeedbackComment({
       navigate("/sociallogin");
     }
   };
-  const onClickModify = () => {};
-  const onClickDelete = (feedback_id) => {
-    axios.delete(`/BE/reference/feedback/${feedback_id}`).then((res) => {
-      console.log(res);
-      axios.get(`/BE/reference/${id3}`).then((res) => {
-        setFeedback(res.data.data.feedbacks);
-      });
-    });
-  };
-
   const onClickThumb = (feedback_id) => {
     axios
-      .post(`/BE/reference/feedback/${feedback_id}/like`)
-      .then((res) => {
-        console.log(res);
-        setFeedback(res.data.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    .post(`/BE/reference/feedback/${feedback_id}/like`)
+    .then((res) => {
+      console.log(res);
+      setFeedback(res.data.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
 
   const onPutHandler = (feedback_id) => {
     const UploadComment = {
@@ -75,74 +65,87 @@ export default function DetailedFeedbackComment({
       .catch((err) => {
         console.log(err);
       });
-  };
+  }
+  const onClickDelete = (feedback_id) => {
+    axios
+    .delete(`/BE/reference/feedback/${feedback_id}`)
+    .then((response) => {
+      console.log(response);
+      setFeedback(response.data.data);
+      alert("댓글 삭제가 완료되었습니다.");
+    })
+    .catch((err) => {
+      alert(err);
+    });
+  }
+  console.log(sessionStorage.getItem("nickname"))
   return (
     <DF.EachFeedWrapper>
-      {feedbacks &&
-        feedbacks.map((feedbacks, index) => (
-          <div style={{ marginBottom: "30px" }} key={index}>
-            <DF.FeedWrapperHeader>
-              <DF.ProfileSize src={feedbacks.member.profileImage} alt="" />
-              <DF.ProfileName>{feedbacks.member.nickname}</DF.ProfileName>
-              <DF.ButtonWrapper>
-                <DF.HeaderButton>
-                  <ThumbUpIcon />
-                  <DF.ThumbCount>{feedbacks.likeCount}</DF.ThumbCount>
+      {feedbacks && feedbacks.map((feedbacks, index) => ( 
+        <div style={{marginBottom:"30px"}} key={index}>
+          <DF.FeedWrapperHeader>
+            <DF.ProfileSize src={feedbacks.member.profileImage} alt="" />
+            <DF.ProfileName>
+              {feedbacks.member.nickname}
+            </DF.ProfileName>
+            <DF.ButtonWrapper>
+              <DF.HeaderButton onClick={() => onClickThumb(feedbacks.feedbackId)}>
+                <ThumbUpIcon />
+                <DF.ThumbCount>{feedbacks.likeCount}</DF.ThumbCount>
+              </DF.HeaderButton>
+              {feedbacks.member.nickname === sessionStorage.getItem("nickname") && (
+              <>
+                <DF.HeaderButton style={{top:"-5.5px",position:"relative",marginLeft:"3px",color:"black"}}
+                onClick={() => {setPutMemberId(feedbacks.feedbackId)}}>
+                  수정
                 </DF.HeaderButton>
-                {feedbacks.member.nickname ===
-                  sessionStorage.getItem("nickname") && (
-                  <>
-                    <DF.HeaderButton
-                      style={{
-                        top: "-5.5px",
-                        position: "relative",
-                        marginLeft: "3px",
-                        color: "black",
-                      }}
-                      onClick={() => {
-                        onPutHandler(feedbacks.feedbackId);
-                        setContents(feedbacks.feedback);
-                      }}
-                    >
-                      수정
-                    </DF.HeaderButton>
-                    <DF.HeaderButton
-                      style={{
-                        top: "-5.5px",
-                        position: "relative",
-                        marginLeft: "3px",
-                        color: "black",
-                      }}
-                      onClick={() => {
-                        onClickDelete(feedbacks.feedbackId);
-                      }}
-                    >
-                      삭제
-                    </DF.HeaderButton>
-                  </>
-                )}
-              </DF.ButtonWrapper>
-            </DF.FeedWrapperHeader>
-            <div>
+                <DF.HeaderButton style={{top:"-5.5px",position:"relative",marginLeft:"3px",color:"black"}}
+                onClick={() => onClickDelete(feedbacks.feedbackId)}>
+                  삭제
+                </DF.HeaderButton>
+              </>)}
+              
+            </DF.ButtonWrapper>          
+          </DF.FeedWrapperHeader>
+            <div >
               <DF.FeedWrapperButton>
-                {link ? (
-                  <DF.WrapperSearch>동영상</DF.WrapperSearch>
-                ) : (
-                  <DF.WrapperSearch href={`#${feedbacks.page}`}>
-                    {feedbacks.page}페이지
-                  </DF.WrapperSearch>
-                )}
+                {link ? 
+                <DF.WrapperSearch>
+                  동영상
+                </DF.WrapperSearch>
+                 :
+                <DF.WrapperSearch href={`#${feedbacks.page-1}`}>
+                {feedbacks.page}페이지
+              </DF.WrapperSearch>
+              }
+               
               </DF.FeedWrapperButton>
-              <p
-                style={{
-                  fontSize: "18px",
-                  lineHeight: "22px",
-                  textAlign: "left",
-                }}
-              >
+              {putMemberId === feedbacks.feedbackId ? (
+                      <div id={feedbacks.feedbackId}>
+                        <br />
+                        <DF.ModifyText
+                          required
+                          placeholder="상세 피드백을 수정하세요."
+                          onChange={onChangeContents}
+                          defaultValue={feedbacks.feedback}
+                        />
+                        <DF.ModifyFin
+                          onClick={() => {
+                            return (
+                              onPutHandler(feedbacks.feedbackId)
+                            );
+                          }}
+                        >
+                          수정 완료하기
+                        </DF.ModifyFin>
+                      </div>
+                    ) : (
+                      <p style={{fontSize:"18px",lineHeight:"22px",textAlign:"left"}}>
                 {feedbacks.feedback}
               </p>
 
+                    )}
+              
               {/* <DetailedFeedbackCommentAgain />*/}
             </div>
           </div>
