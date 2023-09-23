4f5952fbb9bfa2b6f3e20002af4bc0d991648896
geniee1220 <geniee1220@gmail.com>
2023-09-23 14:06:09 +0900

    Feat : 레퍼런스 페이지 상수를 공통 파일로 분리

---
+++ FE/src/containers/reference/constants.js
@@ -0,0 +1,28 @@
+// 카테고리 링크 정보
+export const pageLinks = [
+  { path: '/', text: '기획/아이디어', keyword: 'idea' },
+  { path: '/ref/marketing', text: '광고/마케팅', keyword: 'marketing' },
+  { path: '/ref/video', text: '영상', keyword: 'video' },
+  { path: '/ref/design', text: '디자인/사진', keyword: 'design' },
+  { path: '/ref/etc', text: '기타아이디어', keyword: 'etc' },
+];
+
+// 필터 옵션
+export const filterOptions = [
+  {
+    key: 'newest',
+    value: '최신순',
+  },
+  {
+    key: 'views',
+    value: '조회수순',
+  },
+  {
+    key: 'likes',
+    value: '좋아요순',
+  },
+  {
+    key: 'scrap',
+    value: '스크랩순',
+  },
+];
