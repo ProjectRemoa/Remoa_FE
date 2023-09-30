cc546c33f5350e97ea7b3c6f6157684718bf0268
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-09-30 18:42:41 +0900

    refactor : styles.js 통합, margin-top 수정

--- FE/src/containers/sociallogin/index.jsx
+++ FE/src/containers/sociallogin/index.jsx
@@ -4,7 +4,7 @@ import kakao_login from "../../images/kakao_login_large.png";
 import { KAKAO_AUTH_URL } from "./KakaoLogin/kakaodata";
 import { useEffect } from "react";
 import Modal from "./Modal";
-import S from "./SocialLoginContainer.module.css";
+import S from "./SocialLoginContainer.styles";
 
 function SocialLoginContainer() {
   const navigate = useNavigate();
@@ -26,34 +26,32 @@ function SocialLoginContainer() {
 
   return (
     <>
-      {/*sessionStorage.getItem("new") && */<Modal />}
-      <div className={S.Container}>
-        <div className={S.OuterBox}>
-          <div className={S.TextBox}>
+      {sessionStorage.getItem("new") && <Modal />}
+      <S.Container>
+        <S.OuterBox>
+          <S.TextBox>
             간단한 로그인으로 <br /> 공모전 관련 자료를 자유롭게 찾아보세요
-          </div>
-          <div className={S.InnerBox}>
-            <div className={S.CommentBox}>
-              <p className={S.text1}>
-                공모전 수상작을 포함한 참가 작품들을 자유롭게 열람
+          </S.TextBox>
+          <S.InnerBox>
+            <S.CommentBox>
+              <p>
+                📚 공모전 수상작을 포함한 참가 작품들을 자유롭게 열람
               </p>
-            </div>
-            <div className={S.CommentBox}>
-              <p className={S.text2}>
-                참가작 공유를 통한 다양한 피드백 및 코멘트 수령
-              </p>
-            </div>
-          </div>
-          <div className={S.LoginBox}>
-            <div className={S.LoginInfoBox}>
+            </S.CommentBox>
+            <S.CommentBox>
+              <p>💡 참가작 공유를 통한 다양한 피드백 및 코멘트 수령</p>
+            </S.CommentBox>
+          </S.InnerBox>
+          <S.LoginBox>
+            <S.LoginInfoBox>
               카카오 계정으로 3초만에 가입하기
-            </div>
+            </S.LoginInfoBox>
             <a href={KAKAO_AUTH_URL}>
               <img src={kakao_login} alt="kakaologin" />
             </a>
-          </div>
-        </div>
-      </div>
+          </S.LoginBox>
+        </S.OuterBox>
+      </S.Container>
     </>
   );
 }
