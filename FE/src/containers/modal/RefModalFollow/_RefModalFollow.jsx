0429b4a00b2899ebd02cfb72c5ab71a59c18486f
geniee1220 <geniee1220@gmail.com>
2023-10-09 18:13:28 +0900

    Refactor : 프로필 모달 리팩토링

--- FE/src/containers/modal/RefModalFollow/_RefModalFollow.jsx
+++ FE/src/containers/modal/RefModalFollow/_RefModalFollow.jsx
@@ -1,18 +1,19 @@
-import { MS } from "../../layout/ModalStyle";
-import "../../../src/App.css";
-import { makeStyles } from "@material-ui/core/styles";
-import CloseIcon from "@mui/icons-material/Close";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import axios from "axios";
+import { MS } from '../../../layout/ModalStyle';
+import '../../../src/App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import CloseIcon from '@mui/icons-material/Close';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
+
 const useStyles = makeStyles({
   close: {
-    color: "#FADA5E",
-    position: "absolute",
-    right: "5px",
-    cursor: "pointer",
-    top: "5px",
+    color: '#FADA5E',
+    position: 'absolute',
+    right: '5px',
+    cursor: 'pointer',
+    top: '5px',
   },
 });
 
@@ -28,15 +29,15 @@ export default function RefModalFollow({
   const classes = useStyles();
   const navigate = useNavigate();
   const onCloseHandler = () => {
-    setModalVisibleId("");
+    setModalVisibleId('');
   };
   const [stateFollow, setStateFollow] = useState(false);
   const onClickFollow = () => {
-    console.log("===팔로잉하기 클릭");
+    console.log('===팔로잉하기 클릭');
     //setStatestateFollow(!stateFollow);
-    if (!sessionStorage.getItem("nickname")) {
-      alert("로그인이 필요한 서비스입니다.");
-      navigate("/sociallogin");
+    if (!sessionStorage.getItem('nickname')) {
+      alert('로그인이 필요한 서비스입니다.');
+      navigate('/sociallogin');
     } else {
       // follow 정보 보내고
       axios
@@ -64,11 +65,11 @@ export default function RefModalFollow({
         })
         .catch((err) => {
           console.log(err);
-          if (err.response.data.detail === "자신을 팔로우할 수 없습니다") {
+          if (err.response.data.detail === '자신을 팔로우할 수 없습니다') {
             setIsItMe(true);
-            alert("자신은 팔로우할 수 없습니다.");
-            console.log("자신의 프로필 모달창은 띄우지 않습니다");
-            setModalVisibleId("");
+            alert('자신은 팔로우할 수 없습니다.');
+            console.log('자신의 프로필 모달창은 띄우지 않습니다');
+            setModalVisibleId('');
           }
         });
     }
@@ -82,7 +83,7 @@ export default function RefModalFollow({
   const [follower, setFollower] = useState(0);
   const [isItMe, setIsItMe] = useState(false);
   useEffect(() => {
-    console.log("==팔로잉/팔로우 확인");
+    console.log('==팔로잉/팔로우 확인');
     axios
       .get(`/BE/follow/${memberId}`)
       .then((res) => {
@@ -100,7 +101,7 @@ export default function RefModalFollow({
   return (
     <MS.SmallModalWrapper
       className={
-        modalVisibleId == id && isItMe === false ? "d_block" : "d_none"
+        modalVisibleId == id && isItMe === false ? 'd_block' : 'd_none'
       }
       location={location}
     >
@@ -109,30 +110,30 @@ export default function RefModalFollow({
       </CloseIcon>
       <MS.ModalProfile>
         <MS.ModalProfilePhoto src={idea.postMember.profileImage} />
-        <MS.ModalProfileName style={{ fontSize: "70%" }}>
+        <MS.ModalProfileName style={{ fontSize: '70%' }}>
           {idea.postMember.nickname}
         </MS.ModalProfileName>
       </MS.ModalProfile>
 
       <MS.FollowingFollower>
-        <MS.ModalFollowing style={{ fontSize: "70%" }}>
+        <MS.ModalFollowing style={{ fontSize: '70%' }}>
           Following
-          <span style={{ color: "black" }}>&nbsp;&nbsp;{following}</span>
+          <span style={{ color: 'black' }}>&nbsp;&nbsp;{following}</span>
         </MS.ModalFollowing>
         {/*&nbsp;*/}
       </MS.FollowingFollower>
       <MS.FollowingFollower>
-        <MS.ModalFollower style={{ fontSize: "70%" }}>
-          Follower<span style={{ color: "black" }}>&nbsp;&nbsp;{follower}</span>
+        <MS.ModalFollower style={{ fontSize: '70%' }}>
+          Follower<span style={{ color: 'black' }}>&nbsp;&nbsp;{follower}</span>
         </MS.ModalFollower>
       </MS.FollowingFollower>
 
       <MS.SmallModalButtonWrapper>
         <MS.SmallModalButton onClick={onClickFollow}>
-          {stateFollow ? "언팔로잉하기" : "팔로잉하기"}
+          {stateFollow ? '언팔로잉하기' : '팔로잉하기'}
         </MS.SmallModalButton>
         <MS.SmallModalButton
-          style={{ marginLeft: "7px", cursor: "pointer" }}
+          style={{ marginLeft: '7px', cursor: 'pointer' }}
           onClick={onClickMore}
         >
           더 많은 작품 보기
---
+++ FE/src/containers/modal/RefModalFollow/index.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+
+import StyledComponents from './RefModalFollow.styles';
+import { useNavigate } from 'react-router-dom';
+import { useFollowData } from '../../../apis/references/follow';
+import axios from 'axios';
+const {
+  ProfileModalWrapper,
+  ProfileModalHeader,
+  ProfileImg,
+  ProfileInfoWrapper,
+  ProfileName,
+  ProfileFollowViewWrapper,
+  ProfileFollowView,
+  ProfileFollowButtonWrapper,
+  ProfileFollowButton,
+} = StyledComponents;
+
+function RefModalFollow({ member }) {
+  const navigate = useNavigate();
+
+  const { followData, refetchFollowData } = useFollowData(member.memberId);
+  const [isFollowing, setIsFollowing] = useState(member.isFollow);
+
+  const onClickMemberFeed = (memberId) => {
+    navigate(`/user/list/${memberId}`);
+  };
+
+  const handleMemberFollow = async () => {
+    if (!sessionStorage.getItem('nickname')) {
+      alert('로그인이 필요한 서비스입니다.');
+      return navigate('/sociallogin');
+    }
+
+    try {
+      const response = await axios.post(`/BE/follow/${member.memberId}`);
+      refetchFollowData();
+      setIsFollowing(!isFollowing);
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  };
+
+  return (
+    <ProfileModalWrapper>
+      <ProfileModalHeader>
+        {/* 프로필 이미지 */}
+        <ProfileImg>
+          <img src={member.profileImage} alt="profile image" />
+        </ProfileImg>
+
+        {/* 프로필 정보 */}
+        <ProfileInfoWrapper>
+          <ProfileName>{member.nickname}</ProfileName>
+          <ProfileFollowViewWrapper>
+            <ProfileFollowView>
+              <span>Follower</span>
+              {followData?.follower}
+            </ProfileFollowView>
+            <ProfileFollowView>
+              <span>Following</span>
+              {followData?.following}
+            </ProfileFollowView>
+          </ProfileFollowViewWrapper>
+        </ProfileInfoWrapper>
+      </ProfileModalHeader>
+
+      <ProfileFollowButtonWrapper>
+        {/* 팔로우 버튼 */}
+        {isFollowing ? (
+          <ProfileFollowButton
+            className="followed"
+            onClick={handleMemberFollow}
+          >
+            팔로우 취소
+          </ProfileFollowButton>
+        ) : (
+          <ProfileFollowButton onClick={handleMemberFollow}>
+            팔로잉하기
+          </ProfileFollowButton>
+        )}
+        {/* 유저 피드 바로가기 */}
+        <ProfileFollowButton onClick={() => onClickMemberFeed(member.memberId)}>
+          더 많은 작품 보기
+        </ProfileFollowButton>
+      </ProfileFollowButtonWrapper>
+    </ProfileModalWrapper>
+  );
+}
+
+export default RefModalFollow;
