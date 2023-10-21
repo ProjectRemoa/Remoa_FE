f486a9519b3dd115292bd9d42afa75d92c96b747
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-10-21 23:30:57 +0900

    style : 폰트 Pretendard 수정

--- FE/src/containers/sociallogin/LoginCheck/LoginCheck.styles.js
+++ FE/src/containers/sociallogin/LoginCheck/LoginCheck.styles.js
@@ -5,13 +5,13 @@ const Container =styled.div`
 `
 
 const Button = styled.button`
-  width: 180px;
-  height: 50px;
+  width: 108px;
+  height: 34px;
   background-color: white;
   color: #464646;
   font-family: NotoSansKR-400;
   font-size: 20px;
-  border: none;
+  border: 1px solid #464646;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 50px;
   &:nth-child(2) {
