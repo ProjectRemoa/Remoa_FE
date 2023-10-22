37dc71a0e3221b3ffde7daef9c12d489edf0afa9
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-10-22 13:04:17 +0900

    Refactor : 약관 팝업 UI 수정

--- FE/src/containers/sociallogin/Agree/Agree.styles.js
+++ FE/src/containers/sociallogin/Agree/Agree.styles.js
@@ -1,22 +1,23 @@
 import styled from 'styled-components'
 
 const AgreeBox = styled.div`
-  border: 1px solid #D9D9D9;
+  border: 1px solid #e1e2e5;
+  color: #727272;
   font-size: 13px;
   margin: 0px 20px;
   width: 100%;
   padding: 10px;
   max-height: 180px;
   overflow: auto;
-  ::-webkit-scrollbar{
+  ::-webkit-scrollbar {
     width: 9px;
     border-radius: 30px;
   }
-  ::-webkit-scrollbar-thumb{
+  ::-webkit-scrollbar-thumb {
     background-color: #b0b0b0;
     border-radius: 30px;
   }
-`
+`;
 const S = {
   AgreeBox
 }
