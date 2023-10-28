e29d84a812a8a0fa06ba14fe573f4245121552d2
Nam Min Ki <nam960216@gmail.com>
2023-10-28 14:27:12 +0900

    refactor: MyPageFollow 리팩토링

--- FE/src/containers/mypage/MyPageFollow/MyPageFollow.styles.js
+++ FE/src/containers/mypage/MyPageFollow/MyPageFollow.styles.js
@@ -2,87 +2,64 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   width: 100%;
-  display: flex;
-  flex-direction: column;
+  margin-top: 111px;
+  margin-bottom: 155px;
 `;
 const HeaderWrap = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
 `;
 const BtnWrap = styled.div`
-  width: 50%;
-  float: left;
   display: flex;
-  flex-direction: row;
-  padding-top: 150px;
-  margin: 0 auto;
+  align-items: center;
 `;
-const FollowBtn = styled.a`
+const FollowBtn = styled.button`
   width: auto;
+  padding: 0;
   font-size: 25px;
+  font-weight: 700;
   border: none;
-  background-color: transparent;
-  color: #000000;
-  text-align: left;
-  outline: none;
   box-shadow: none;
-  cursor: pointer;
+  background-color: transparent;
 `;
 const Line = styled.div`
   font-size: 25px;
-  color: #000000;
   text-align: center;
   font-weight: bold;
-  padding: 0 10px 0 10px;
+  padding: 0 10px;
 `;
 const FollowNum = styled.div`
-  width: 50%;
-  float: left;
   display: flex;
-  padding-top: 15px;
-  margin: 0 auto;
+  padding-top: 12px;
 `;
 const Text = styled.span`
   color: #fada5e;
-  font-weight: bold;
-`;
-const FollowList = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+  font-weight: 700;
 `;
 const FollowWrap = styled.div`
-  width: 50%;
+  width: 100%;
   display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding-top: 60px;
-  margin: 0 auto;
+  justify-content: left;
+  padding-top: 44px;
 `;
 const FollowProfileImgWrap = styled.div`
-  width: 10%;
-  padding: 0 20px 0 0;
+  width: 20%;
 `;
 const FollowProfileImg = styled.img`
-  width: 160px;
-  height: 160px;
+  width: 114px;
+  height: 114px;
   border-radius: 50%;
-  overflow: hidden;
 `;
 const FollowProfileIntroWrap = styled.div`
-  width: 50%;
+  width: 60%;
   display: flex;
   flex-direction: column;
-  float: left;
-  margin: 0 auto;
-  text-align: left;
+  align-items: start;
+  justify-content: start;
 `;
 const FollowProfileName = styled.div`
   font-size: 30px;
-  color: #000000;
 `;
 const FollowNumWrap = styled.div`
   display: flex;
@@ -90,32 +67,31 @@ const FollowNumWrap = styled.div`
   margin-top: 10px;
 `;
 const FollowTotal = styled.div`
-  color: #000000;
   font-size: 20px;
-  padding: 0 10px 0 0;
+  margin-right: 10px;
 `;
 const FollowTitle = styled.span`
   color: #fada5e;
-  font-size: 20px;
-  font-weight: bold;
-  padding: 0 10px 0 0;
+  font-weight: 700;
 `;
 const FollowProfileIntro = styled.div`
-  width: 100%;
   color: #1f1f1f;
   font-size: 20px;
-  margin-top: 20px;
+  margin-top: 15px;
 `;
 const FollowBtnWrap = styled.div`
+  width: 20%;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: space-between;
 `;
 const FollowDetailBtn = styled.button`
   width: 173px;
   height: 45px;
   background: #fada5e;
   border-radius: 10px;
+  box-shadow: none;
+  border: none;
   color: #000000;
 `;
 
@@ -127,7 +103,6 @@ const styledComponent = {
   Line,
   FollowNum,
   Text,
-  FollowList,
   FollowWrap,
   FollowProfileImgWrap,
   FollowProfileImg,
--- FE/src/containers/mypage/MyPageFollow/index.jsx
+++ FE/src/containers/mypage/MyPageFollow/index.jsx
@@ -10,7 +10,6 @@ const {
   Line,
   FollowNum,
   Text,
-  FollowList,
   FollowWrap,
   FollowProfileImgWrap,
   FollowProfileImg,
@@ -26,25 +25,8 @@ const {
 
 function MyPageFollow() {
   const navigate = useNavigate();
-  const [followingList, setFollowingList] = useState([]);
-  const [followerList, setFollowerList] = useState([]);
-  const [following, setFollowing] = useState(true);
-  const [followInfo, setFollowInfo] = useState({
-    // followingColor: "#000000",
-    // followerColor: "#D0D0D0",
-    // followingIntro: (
-    //   <FollowNum>
-    //     <Text>호갱</Text>님이 팔로우하고 있는 유저는 총&nbsp;
-    //     <Text>10</Text>명 입니다
-    //   </FollowNum>
-    // ),
-    // followerIntro: (
-    //   <FollowNum>
-    //     총&nbsp;<Text>10</Text>명이&nbsp;
-    //     <Text>호갱</Text>님을 팔로잉하고 있어요
-    //   </FollowNum>
-    // ),
-  });
+  const [followList, setFollowList] = useState([]);
+  const [followInfo, setFollowInfo] = useState({});
 
   const checkFollow = (memberId) => {
     axios
@@ -58,71 +40,67 @@ function MyPageFollow() {
       });
   };
 
-  const getFollowingList = () => {
-    const newInfo = [];
+  const UserInfo = ({ i, user, type, checkFollow }) => (
+    <FollowWrap key={i}>
+      <FollowProfileImgWrap>
+        <FollowProfileImg src={user.profileImage} />
+      </FollowProfileImgWrap>
+
+      <FollowProfileIntroWrap>
+        <FollowProfileName>{user.userName}</FollowProfileName>
+        <FollowNumWrap>
+          <FollowTotal>
+            <FollowTitle>Follower</FollowTitle> {user.followerNum}
+          </FollowTotal>
+          <FollowTotal>
+            <FollowTitle>Following</FollowTitle> {user.followingNum}
+          </FollowTotal>
+        </FollowNumWrap>
+        <FollowProfileIntro>{user.oneLineIntroduction}</FollowProfileIntro>
+      </FollowProfileIntroWrap>
+
+      <FollowBtnWrap>
+        <FollowDetailBtn onClick={() => checkFollow(user.memberId)}>
+          {type === "following" ? "팔로잉 취소" : "맞팔로우 하기"}
+        </FollowDetailBtn>
+        <FollowDetailBtn
+          onClick={() => navigate(`/user/list/${user.memberId}`)}
+        >
+          작품 보러가기
+        </FollowDetailBtn>
+      </FollowBtnWrap>
+    </FollowWrap>
+  );
+
+  const getUserInfo = (type) => {
     axios
-      .get(`/BE/following`)
+      .get(`/BE/${type}`)
       .then((res) => {
-        for (let i = 0; i < res.data.data.resMypageList.length; i++) {
-          let userInfo = (
-            <FollowWrap key={i.toString()}>
-              <FollowProfileImgWrap>
-                <FollowProfileImg
-                  src={res.data.data.resMypageList[i].profileImage}
-                ></FollowProfileImg>
-              </FollowProfileImgWrap>
-              <FollowProfileIntroWrap>
-                <FollowProfileName>
-                  {res.data.data.resMypageList[i].userName}
-                </FollowProfileName>
-                <FollowNumWrap>
-                  <FollowTotal>
-                    <FollowTitle>Follower</FollowTitle>{" "}
-                    {res.data.data.resMypageList[i].followerNum}
-                  </FollowTotal>
-                  <FollowTotal>
-                    <FollowTitle>Following</FollowTitle>{" "}
-                    {res.data.data.resMypageList[i].followingNum}
-                  </FollowTotal>
-                </FollowNumWrap>
-                <FollowProfileIntro>
-                  {res.data.data.resMypageList[i].oneLineIntroduction}
-                </FollowProfileIntro>
-              </FollowProfileIntroWrap>
-              <FollowBtnWrap>
-                <FollowDetailBtn
-                  onClick={() =>
-                    checkFollow(res.data.data.resMypageList[i].memberId)
-                  }
-                >
-                  팔로잉 취소
-                </FollowDetailBtn>
-                <FollowDetailBtn
-                  onClick={() =>
-                    navigate(
-                      `/user/list/${res.data.data.resMypageList[i].memberId}`
-                    )
-                  }
-                >
-                  작품 보러가기
-                </FollowDetailBtn>
-              </FollowBtnWrap>
-            </FollowWrap>
-          );
-          newInfo.push(userInfo);
-        }
-        setFollowingList(newInfo);
+        const newInfo = res.data.data.resMypageList.map((user, i) => (
+          <UserInfo key={i} user={user} type={type} checkFollow={checkFollow} />
+        ));
+        setFollowList(newInfo);
         setFollowInfo({
-          followingColor: "#000000",
-          followerColor: "#D0D0D0",
-          followingIntro: (
+          followColor: type === "following" ? "#000000" : "#D0D0D0",
+          followerColor: type === "following" ? "#D0D0D0" : "#000000",
+          followIntro: (
             <FollowNum>
-              <Text>{res.data.data.userName}</Text>님이 팔로우하고 있는 유저는
-              총&nbsp;
-              <Text>{res.data.data.followNum}</Text>명 입니다
+              {type === "following" ? (
+                <>
+                  <Text>{res.data.data.userName}</Text>님이 팔로우하고 있는
+                  유저는 총&nbsp;
+                  <Text>{res.data.data.followNum}</Text>명 입니다
+                </>
+              ) : (
+                <>
+                  총&nbsp;
+                  <Text>{res.data.data.followNum}</Text>
+                  명이&nbsp;
+                  <Text>{res.data.data.userName}</Text>님을 팔로잉하고 있어요
+                </>
+              )}
             </FollowNum>
           ),
-          followerIntro: null,
         });
       })
       .catch((err) => {
@@ -130,109 +108,36 @@ function MyPageFollow() {
       });
   };
 
-  const getFollowerList = () => {
-    const newInfo = [];
-    axios.get(`/BE/follower`).then((res) => {
-      for (let i = 0; i < res.data.data.resMypageList.length; i++) {
-        let userInfo = (
-          <FollowWrap key={i.toString()}>
-            <FollowProfileImgWrap>
-              <FollowProfileImg
-                src={res.data.data.resMypageList[i].profileImage}
-              ></FollowProfileImg>
-            </FollowProfileImgWrap>
-            <FollowProfileIntroWrap>
-              <FollowProfileName>
-                {res.data.data.resMypageList[i].userName}
-              </FollowProfileName>
-              <FollowNumWrap>
-                <FollowTotal>
-                  <FollowTitle>Follower</FollowTitle>{" "}
-                  {res.data.data.resMypageList[i].followerNum}
-                </FollowTotal>
-                <FollowTotal>
-                  <FollowTitle>Following</FollowTitle>{" "}
-                  {res.data.data.resMypageList[i].followingNum}
-                </FollowTotal>
-              </FollowNumWrap>
-              <FollowProfileIntro>
-                {res.data.data.resMypageList[i].oneLineIntroduction}
-              </FollowProfileIntro>
-            </FollowProfileIntroWrap>
-            <FollowBtnWrap>
-              <FollowDetailBtn
-                onClick={() =>
-                  checkFollow(res.data.data.resMypageList[i].memberId)
-                }
-              >
-                맞팔로우 하기
-              </FollowDetailBtn>
-              <FollowDetailBtn
-                onClick={() =>
-                  navigate(
-                    `/user/list/${res.data.data.resMypageList[i].memberId}`
-                  )
-                }
-              >
-                작품 보러가기
-              </FollowDetailBtn>
-            </FollowBtnWrap>
-          </FollowWrap>
-        );
-        newInfo.push(userInfo);
-      }
-      setFollowerList(newInfo);
-      setFollowInfo({
-        followingColor: "#D0D0D0",
-        followerColor: "#000000",
-        followingIntro: null,
-        followerIntro: (
-          <FollowNum>
-            총&nbsp;<Text>{res.data.data.followNum}</Text>
-            명이&nbsp;
-            <Text>{res.data.data.userName}</Text>님을 팔로잉하고 있어요
-          </FollowNum>
-        ),
-      });
-    });
-  };
-
   useEffect(() => {
-    getFollowingList();
+    getUserInfo("following");
   }, []);
 
   return (
-    <>
-      <Wrapper>
-        <HeaderWrap>
-          <BtnWrap>
-            <FollowBtn
-              style={{ color: followInfo.followingColor, fontWeight: "bolder" }}
-              onClick={() => {
-                setFollowing(true);
-                getFollowingList();
-              }}
-            >
-              팔로잉 관리
-            </FollowBtn>
-            <Line>|</Line>
-            <FollowBtn
-              style={{ color: followInfo.followerColor, fontWeight: "bolder" }}
-              onClick={() => {
-                setFollowing(false);
-                getFollowerList();
-              }}
-            >
-              팔로워 관리
-            </FollowBtn>
-          </BtnWrap>
-          {following ? followInfo.followingIntro : followInfo.followerIntro}
-        </HeaderWrap>
-        <FollowList>
-          {following === true ? followingList : followerList}
-        </FollowList>
-      </Wrapper>
-    </>
+    <Wrapper>
+      <HeaderWrap>
+        <BtnWrap>
+          <FollowBtn
+            style={{ color: followInfo.followColor }}
+            onClick={() => {
+              getUserInfo("following");
+            }}
+          >
+            팔로잉 관리
+          </FollowBtn>
+          <Line>|</Line>
+          <FollowBtn
+            style={{ color: followInfo.followerColor }}
+            onClick={() => {
+              getUserInfo("follower");
+            }}
+          >
+            팔로워 관리
+          </FollowBtn>
+        </BtnWrap>
+        {followInfo.followIntro}
+      </HeaderWrap>
+      {followList}
+    </Wrapper>
   );
 }
 
