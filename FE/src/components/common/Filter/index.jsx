cc782bdef77c474e2ba7d221cf60a496ca9efbc4
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-11-05 00:51:13 +0900

    Refactor : Filter 드롭다운

---
+++ FE/src/components/common/Filter/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { filterOptions } from '../../../containers/reference/constants'
+
+function Filter() {
+  return (
+    <div>
+      필터입니다
+    </div>
+  )
+}
+
+export default Filter
