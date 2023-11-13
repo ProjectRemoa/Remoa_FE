8e071d892aeaf4be03ba61c8146478bbf134524c
Nam Min Ki <nam960216@gmail.com>
2023-11-13 15:43:44 +0900

    fix: 프로필 관리 페이지 개발

--- FE/src/containers/mypage/MyPageFollow/MyPageFollow.styles.js
+++ FE/src/containers/mypage/MyPageFollow/MyPageFollow.styles.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  width: 100%;
-  margin-top: 111px;
-  margin-bottom: 155px;
+  width: 900px;
+  box-sizing: border-box;
+  padding: 0px 30px;
+  margin: 65px 0px 155px;
 `;
 const HeaderWrap = styled.div`
   width: 100%;
@@ -15,84 +16,100 @@ const BtnWrap = styled.div`
   align-items: center;
 `;
 const FollowBtn = styled.button`
+  font-family: Pretendard-Medium;
   width: auto;
   padding: 0;
   font-size: 25px;
-  font-weight: 700;
   border: none;
   box-shadow: none;
   background-color: transparent;
 `;
 const Line = styled.div`
-  font-size: 25px;
+  font-size: 29px;
+  color: #d9d9d9;
   text-align: center;
-  font-weight: bold;
-  padding: 0 10px;
+  margin: 0 10px;
 `;
 const FollowNum = styled.div`
-  display: flex;
-  padding-top: 12px;
+  text-align: left;
+  margin-top: 15px;
+  font-family: Pretendard-SemiBold;
 `;
 const Text = styled.span`
-  color: #fada5e;
-  font-weight: 700;
+  color: #f24e1e;
 `;
 const FollowWrap = styled.div`
   width: 100%;
   display: flex;
-  justify-content: left;
-  padding-top: 44px;
+  justify-content: space-between;
+  margin: 24px 0px;
 `;
 const FollowProfileImgWrap = styled.div`
-  width: 20%;
+  width: 15%;
+  text-align: left;
 `;
 const FollowProfileImg = styled.img`
-  width: 114px;
-  height: 114px;
+  width: 80px;
+  height: 80px;
   border-radius: 50%;
 `;
 const FollowProfileIntroWrap = styled.div`
-  width: 60%;
+  width: 75%;
   display: flex;
   flex-direction: column;
   align-items: start;
-  justify-content: start;
 `;
 const FollowProfileName = styled.div`
-  font-size: 30px;
+  font-family: Pretendard-SemiBold;
+  font-size: 20px;
 `;
 const FollowNumWrap = styled.div`
   display: flex;
-  flex-direction: row;
-  margin-top: 10px;
+  align-items: center;
+  margin: 10px 0px;
 `;
 const FollowTotal = styled.div`
   font-size: 20px;
-  margin-right: 10px;
+  &:nth-child(2) {
+    margin-left: 12px;
+  }
 `;
 const FollowTitle = styled.span`
-  color: #fada5e;
-  font-weight: 700;
+  color: #b0b0b0;
+  font-weight: 500;
+  font-size: 13px;
 `;
 const FollowProfileIntro = styled.div`
-  color: #1f1f1f;
-  font-size: 20px;
-  margin-top: 15px;
+  color: #727272;
+  font-size: 16px;
 `;
 const FollowBtnWrap = styled.div`
   width: 20%;
   display: flex;
   flex-direction: column;
+  align-items: end;
   justify-content: space-between;
 `;
 const FollowDetailBtn = styled.button`
-  width: 173px;
-  height: 45px;
-  background: #fada5e;
-  border-radius: 10px;
+  width: 132px;
+  height: 36px;
+  background-color: ${(props) =>
+    props.isFollowing ? "#fada5e" : props.isFollowed ? "#f0f0f0" : "#fada5e"};
+  border-radius: 8px;
   box-shadow: none;
   border: none;
-  color: #000000;
+  font-family: Pretendard-SemiBold;
+  color: #1e1e1e;
+  &:nth-child(2) {
+    border: 1px solid #e1e2e5;
+    background-color: white;
+  }
+`;
+const HorizonLine = styled.hr`
+  width: 100%;
+  height: 1px;
+  background-color: #e1e2e5;
+  border: 0;
 `;
 
 const styledComponent = {
@@ -114,6 +131,7 @@ const styledComponent = {
   FollowProfileIntro,
   FollowBtnWrap,
   FollowDetailBtn,
+  HorizonLine,
 };
 
 export default styledComponent;
