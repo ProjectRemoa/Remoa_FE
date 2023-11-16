953ac9e74962c8dcf418aeb22dcc36971ed023a6
Nam Min Ki <nam960216@gmail.com>
2023-11-16 14:47:46 +0900

    fix: 내 활동 관리 수정

--- FE/src/containers/mypage/MyPageWorkContainer/MyPageWorkContainer.styles.js
+++ FE/src/containers/mypage/MyPageWorkContainer/MyPageWorkContainer.styles.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
+  width: 65vw;
   display: flex;
   flex-direction: column;
-  margin: 100px 0px 55px;
+  margin: 61px 0 55px;
 `;
 
 const CommentContainer = styled.div`
@@ -12,9 +13,60 @@ const CommentContainer = styled.div`
   text-align: left;
 `;
 
+const MoreButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 54px;
+`;
+
+const MoreButton = styled.button`
+  width: 160px;
+  height: 48px;
+  border-radius: 10px;
+  border: 1px solid #b0b0b0;
+  color: #464646;
+  background-color: transparent;
+  font-size: 16px;
+  font-family: Pretendard-SemiBold;
+`;
+
+const ScrapContainer = styled.div`
+  width: 100%;
+  font-size: 25px;
+  font-weight: 700;
+  text-align: left;
+  margin-top: 96px;
+`;
+const CategoryContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+const CategoryButton = styled.button`
+  width: 15.8%;
+  height: 44px;
+  border: 1px solid #cecece;
+  border-radius: 10px;
+  background-color: ${(props) => (props.clicked ? "#fada5e" : "transparent")};
+`;
+const ScrapListContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 48px 24px;
+  margin-top: 40px;
+`;
+
 const styledComponent = {
   Wrapper,
   CommentContainer,
+  MoreButtonContainer,
+  MoreButton,
+  ScrapContainer,
+  CategoryContainer,
+  CategoryButton,
+  ScrapListContainer,
 };
 
 export default styledComponent;
