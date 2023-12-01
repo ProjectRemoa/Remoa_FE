5f0543031ba2b3feff2ecc19be3fa44bbfd99052
Nam Min Ki <nam960216@gmail.com>
2023-12-01 15:07:09 +0900

    fix: faq react-query 삭제

--- FE/src/containers/mypage/MyPageFAQ/MyPageFAQ.styles.js
+++ FE/src/containers/mypage/MyPageFAQ/MyPageFAQ.styles.js
@@ -1,12 +1,46 @@
+import ReactPaginate from "react-paginate";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
   width: 65%;
-  margin-top: 58px;
+  margin: 58px 0 186px;
+`;
+
+const MyPaginate = styled(ReactPaginate)`
+  margin: 44px 0 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  list-style: none;
+
+  li {
+    margin-right: 8px;
+    font-weight: 500;
+    font-size: 15px;
+    color: #727272;
+    cursor: pointer;
+  }
+
+  li.previous,
+  li.next {
+    color: #000000;
+  }
+
+  li.selected a {
+    font-weight: 800;
+    font-size: 15px;
+    color: #1e1e1e;
+    cursor: default;
+  }
+
+  li.disabled {
+    cursor: default;
+  }
 `;
 
 const styledComponent = {
   Wrapper,
+  MyPaginate,
 };
 
 export default styledComponent;
--- FE/src/containers/mypage/MyPageFAQ/index.jsx
+++ FE/src/containers/mypage/MyPageFAQ/index.jsx
@@ -1,20 +1,50 @@
-import { useQuery } from "react-query";
-import { getNotices, getInquiries } from "../../../apis/mypage/faq";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import TableComponent from "../../../components/common/TableComponent";
 import styledComponent from "./MyPageFAQ.styles";
-const { Wrapper } = styledComponent;
+const { Wrapper, MyPaginate } = styledComponent;
 
 function MyPageFAQ() {
-  const { data: noticeData } = useQuery("noticeData", getNotices);
-  const { data: inquiryData } = useQuery("inquryData", getInquiries);
+  const [noticePage, setNoticePage] = useState(1);
+  const [noticeData, setNoticeData] = useState([]);
+  const [noticeTotalPage, setNoticeTotalPage] = useState();
+
+  const [inquiryPage, setInquiryPage] = useState(1);
+  const [inquiryData, setInquiryData] = useState([]);
+  const [inquiryTotalPage, setInquiryTotalPage] = useState();
+
+  useEffect(() => {
+    axios.get(`/BE/notice?page=${noticePage}`).then((res) => {
+      setNoticeData(res.data.data.notices);
+      setNoticeTotalPage(res.data.data.totalPages);
+    });
+
+    axios.get(`/BE/inquiry?page=${inquiryPage}`).then((res) => {
+      setInquiryData(res.data.data.inquiries);
+      setInquiryTotalPage(res.data.data.totalPages);
+    });
+  }, [noticePage, inquiryPage]);
 
   return (
     <Wrapper>
       <TableComponent title="공지사항" data={noticeData} category={"notice"} />
+      <MyPaginate
+        pageCount={noticeTotalPage}
+        previousLabel="<"
+        nextLabel=">"
+        onPageChange={(e) => setNoticePage(e.selected + 1)}
+      />
+
       <TableComponent
-        title="문의하기"
+        title="문의사항"
         data={inquiryData}
         category={"inquiry"}
+      />
+      <MyPaginate
+        pageCount={inquiryTotalPage}
+        previousLabel="<"
+        nextLabel=">"
+        onPageChange={(e) => setInquiryPage(e.selected + 1)}
       />
     </Wrapper>
   );
