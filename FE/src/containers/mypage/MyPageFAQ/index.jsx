2849243daed49d4987202bde60a612d7c15bd39e
Nam Min Ki <nam960216@gmail.com>
2023-12-15 17:52:25 +0900

    refactor: prefetchQuery 적용

--- FE/src/containers/mypage/MyPageFAQ/index.jsx
+++ FE/src/containers/mypage/MyPageFAQ/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import { getNotices, getInquiries } from "../../../apis/mypage/faq";
 import Loading from "../../../styles/Loading";
 import TableComponent from "../../../components/common/TableComponent";
@@ -25,29 +25,38 @@ const filterOptions = [
 ];
 
 function MyPageFAQ() {
-  const { data: noticeData, isLoading: isNoticeLoading } = useQuery(
-    ["noticeData"],
-    getNotices
-  );
-  const { data: inquiryData, isLoading: isInquiryLoading } = useQuery(
-    ["inquryData"],
-    getInquiries
-  );
-
+  const queryClient = useQueryClient();
   const [input, setInput] = useState("");
   const [noticePage, setNoticePage] = useState(1);
-  const [noticeTotalPage, setNoticeTotalPage] = useState();
-
   const [inquiryPage, setInquiryPage] = useState(1);
-  const [inquiryTotalPage, setInquiryTotalPage] = useState();
-
   const [filter, setFilter] = useState(filterOptions[0].value);
   const [sortOption, setSortOption] = useState(filterOptions[0].key);
 
   useEffect(() => {
     setSortOption(filterOptions[0].key);
     setFilter(filterOptions[0].value);
-  }, []);
+
+    const nextNoticePage = noticePage + 1;
+    const nextInquiryPage = inquiryPage + 1;
+
+    queryClient.prefetchQuery(["nextNoticePage", nextNoticePage], () =>
+      getNotices(nextNoticePage)
+    );
+    queryClient.prefetchQuery(["nextInquiryPage", nextInquiryPage], () =>
+      getInquiries(nextInquiryPage)
+    );
+  }, [noticePage, inquiryPage, queryClient]);
+
+  const { data: noticeData, isLoading: isNoticeLoading } = useQuery(
+    ["noticeData", noticePage],
+    () => getNotices(noticePage),
+    { keepPreviousData: true }
+  );
+  const { data: inquiryData, isLoading: isInquiryLoading } = useQuery(
+    ["inquryData", inquiryPage],
+    () => getInquiries(inquiryPage),
+    { keepPreviousData: true }
+  );
 
   const handleInput = (input) => {
     setInput(input);
@@ -61,47 +70,55 @@ function MyPageFAQ() {
     }
   };
 
-  return isNoticeLoading || isInquiryLoading ? (
-    <Loading />
-  ) : (
-    <Wrapper>
-      <TableComponent title="공지사항" data={noticeData} category={"notice"} />
-      <MyPaginate
-        pageCount={noticeTotalPage}
-        previousLabel="<"
-        nextLabel=">"
-        onPageChange={(e) => setNoticePage(e.selected + 1)}
-      />
+  return (
+    <>
+      {isNoticeLoading || isInquiryLoading ? (
+        <Loading />
+      ) : (
+        <Wrapper>
+          <TableComponent
+            title="공지사항"
+            data={noticeData?.notices}
+            category={"notice"}
+          />
+          <MyPaginate
+            pageCount={noticeData?.totalPages}
+            previousLabel="<"
+            nextLabel=">"
+            onPageChange={(e) => setNoticePage(e.selected + 1)}
+          />
 
-      <TableComponent
-        title="문의사항"
-        data={inquiryData}
-        category={"inquiry"}
-      />
-      <MyPaginate
-        pageCount={inquiryTotalPage}
-        previousLabel="<"
-        nextLabel=">"
-        onPageChange={(e) => setInquiryPage(e.selected + 1)}
-      />
+          <TableComponent
+            title="문의사항"
+            data={inquiryData?.inquiries}
+            category={"inquiry"}
+          />
+          <MyPaginate
+            pageCount={inquiryData?.totalPages}
+            previousLabel="<"
+            nextLabel=">"
+            onPageChange={(e) => setInquiryPage(e.selected + 1)}
+          />
 
-      <SearchWrapper>
-        <Dropdown
-          filter={filter}
-          setFilter={setFilter}
-          setSortOption={setSortOption}
-        />
-        {"|"}
-        <SearchInput
-          placeholder="검색어를 입력해주세요"
-          onChange={(e) => handleInput(e.target.value)}
-          onKeyDown={(e) => onKeyDownSearch(e)}
-        />
-        <SearchIcon type="button">
-          <FaMagnifyingGlass onClick={handleClick} color="#a7a7a7" />
-        </SearchIcon>
-      </SearchWrapper>
-    </Wrapper>
+          <SearchWrapper>
+            <Dropdown
+              filter={filter}
+              setFilter={setFilter}
+              setSortOption={setSortOption}
+              filterOptions={filterOptions}
+            />
+            <SearchInput
+              placeholder="검색어를 입력해주세요"
+              onChange={(e) => handleInput(e.target.value)}
+              onKeyDown={(e) => onKeyDownSearch(e)}
+            />
+            <SearchIcon type="button">
+              <FaMagnifyingGlass onClick={handleClick} color="#a7a7a7" />
+            </SearchIcon>
+          </SearchWrapper>
+        </Wrapper>
+      )}
+    </>
   );
 }
 
