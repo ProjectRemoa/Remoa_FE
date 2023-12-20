23092d49eeb3eb1e329b9aef565097134c4460aa
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-12-20 14:49:30 +0900

    Feat : Proxy 설정

--- FE/src/setupProxy.js
+++ FE/src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
+
+  
   app.use(
     "/BE",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "http://54.180.159.30:8080",
       changeOrigin: true,
       pathRewrite: {
         "^/BE": "", // URL ^/BE -> 공백 변경
