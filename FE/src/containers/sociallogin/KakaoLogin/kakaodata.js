73ac498271d53bf6a2694815c35b790237684f71
Nam Min Ki <nam960216@gmail.com>
2023-12-23 16:17:37 +0900

    fix: redirect_uri 변경

--- FE/src/containers/sociallogin/KakaoLogin/kakaodata.js
+++ FE/src/containers/sociallogin/KakaoLogin/kakaodata.js
@@ -1,4 +1,7 @@
 export const CLIENT_ID = process.env.REACT_APP_REST_API_KEY;
-export const REDIRECT_URL = "http://localhost:3000/login/kakao";
+export const REDIRECT_URI =
+  process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_DEV_REDIRECT_URI
+    : process.env.REACT_APP_PROD_REDIRECT_URI;
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=139febf9e13da4d124d1c1faafcf3f86&redirect_uri=http://localhost:3000/login/kakao&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
