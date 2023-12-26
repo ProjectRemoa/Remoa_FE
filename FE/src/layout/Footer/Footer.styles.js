3afcd44ca431ac26a81eac3ec51aeaf011419166
geniee1220 <geniee1220@gmail.com>
2023-12-26 11:03:32 +0900

    Design : 푸터에 reset 스타일 추가

--- FE/src/layout/Footer/Footer.styles.js
+++ FE/src/layout/Footer/Footer.styles.js
@@ -4,8 +4,13 @@ const FooterContainer = styled.footer`
   display: flex;
   justify-content: center;
   width: 100%;
+  height: 238px;
+  max-height: 238px;
   padding: 44px 0 48px;
   background: #f5f5f5;
+
+  // reset.css
+  box-sizing: border-box;
 `;
 
 const FooterInner = styled.div`
