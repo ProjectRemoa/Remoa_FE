16e61cc6ed6ab37cb4695406bd076a9144136229
geniee1220 <geniee1220@gmail.com>
2023-12-26 09:40:56 +0900

    Refactor : 헤더 컴포넌트 UI 수정 및 리팩토링

--- FE/src/layout/Header/Header.styles.js
+++ FE/src/layout/Header/Header.styles.js
@@ -1,6 +1,7 @@
-import styled from "styled-components";
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
-const Header = styled.nav`
+export const Header = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -10,36 +11,8 @@ const Header = styled.nav`
   background-color: #fada5e;
   z-index: 3;
 `;
-const Contents = styled.div`
-  display: flex;
-  align-items: center;
-`;
-const Items = styled.ul`
-  display: flex;
-  align-items: center;
-  list-style: none;
-`;
-const Item = styled.li`
-  font-weight: bold;
-  font-size: 20px;
-  margin-right: 30px;
-  position: relative;
-  a {
-    color: black;
-    text-decoration: none;
-    /* text-decoration-color: white;
-      text-decoration-thickness: 3px;
-      text-underline-offset: 6px; */
-  }
-  @media ${(props) => props.theme.desktop} {
-  }
-  @media ${(props) => props.theme.mobile} {
-    display: none;
-  }
-`;
-const Underline = styled.span``;
 
-const Logo = styled.a`
+export const Logo = styled.a`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -47,82 +20,55 @@ const Logo = styled.a`
   img {
     width: 120px;
   }
-  @media ${(props) => props.theme.desktop} {
-  }
-  @media ${(props) => props.theme.mobile} {
-  }
 `;
 
-const HoverlineUnder1 = styled.div`
-  width: 75px;
-  border: 2px solid #ffffff;
-  position: absolute;
-  top: 30px;
-`;
-const HoverlineUnder2 = styled.div`
-  width: 100px;
-  border: 2px solid #ffffff;
-  position: absolute;
-  top: 30px;
-`;
-const HoverlineUnder3 = styled.div`
-  width: 95px;
-  border: 2px solid #ffffff;
-  position: absolute;
-  top: 30px;
-`;
-
-const BellButton = styled.button`
-  border: solid 2px white;
-  width: 3rem;
-  height: 3rem;
-  background: #ffffff;
-  border-radius: 50px;
-  cursor: pointer;
-  position: relative;
-`;
-const BellNotice = styled.div`
-  position: absolute;
-  width: 10px;
-  height: 10px;
-  background: #f27a7a;
-  border-radius: 50%;
-  border: solid 0.5px white;
-  left: 28px;
-  top: 7px;
-`;
-const StyledButton = styled.button`
-  width: 10rem;
-  height: 3rem;
-  background-color: white;
-  color: #464646;
-  font-size: 15px;
-  border: solid 2px white;
-  cursor: pointer;
-  letter-spacing: 0.1em;
-  font-weight: 550;
+export const HeaderInner = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
-  background: #ffffff;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 50px;
-  margin-left: 10px;
 `;
 
-const S = {
-  Header,
-  Contents,
-  Items,
-  Item,
-  Underline,
-  Logo,
-  HoverlineUnder1,
-  HoverlineUnder2,
-  HoverlineUnder3,
-  BellButton,
-  BellNotice,
-  StyledButton,
-};
+export const Items = styled.ul`
+  // reset.css
+  margin: 0;
+  padding: 0;
+  list-style: none;
 
-export default S;
+  display: flex;
+  align-items: center;
+  margin-left: 120px;
+  gap: 0 64px;
+
+  @media ${(props) => props.theme.desktop} {
+    margin-left: 60px;
+    gap: 0 40px;
+  }
+  @media ${(props) => props.theme.mobile} {
+    margin-left: 30px;
+  }
+`;
+
+export const Item = styled.li`
+  font-weight: bold;
+  font-size: 16px;
+
+  a {
+    color: black;
+    text-decoration: none;
+  }
+`;
+
+export const StyledLink = styled(NavLink)`
+  &.active {
+    position: relative;
+
+    &::after {
+      content: '';
+      position: absolute;
+      top: calc(100% + 6px);
+      left: 50%;
+      transform: translateX(-50%);
+      width: calc(100% + 22px);
+      border-bottom: 4px solid #ffffff;
+    }
+  }
+`;
--- FE/src/layout/Header/index.jsx
+++ FE/src/layout/Header/index.jsx
@@ -1,91 +1,32 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import img from "../../images/LEMOA_TEXT_MARK.png";
-import LoginCheck from "../../containers/sociallogin/LoginCheck";
-import S from "./Header.styles";
+import img from '../../images/LEMOA_TEXT_MARK.png';
+import LoginCheck from '../../containers/sociallogin/LoginCheck';
+import * as S from './Header.styles';
+
+const CATEGORY_LINKS = [
+  { name: '레퍼런스', link: '/' },
+  { name: '작업물 관리', link: '/manage/list' },
+  { name: '마이페이지', link: '/mypage/profile' },
+];
 
 function Header() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [isLogin, setIsLogin] = useState(false);
-
-  useEffect(() => {
-    if (sessionStorage.getItem("id") !== null) {
-      setIsLogin(true);
-    } else {
-      setIsLogin(false);
-    }
-  }, []);
-
-  const noticeOnClick = () => {};
-
-  const loginOnClick = () => {
-    navigate("/login");
-  };
-
-  const logoutOnClick = () => {
-    axios.defaults.withCredentials = true;
-    // 404 error 발생
-    axios
-      .post(`/user/logout`, {
-        withCredentials: true,
-      })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    sessionStorage.removeItem("id");
-    navigate("/");
-  };
-
-  const bellOnClick = () => {};
-
-  const shareOnClick = () => {};
-
-  const signupOnClick = () => {
-    navigate("/signup");
-  };
   return (
     <S.Header>
-      <S.Contents>
+      <S.HeaderInner>
+        <S.Logo href="/">
+          <img src={img} alt="Remoa Logo" />
+        </S.Logo>
+
         <S.Items>
-          <S.Item>
-            <S.Logo href="/">
-              <img src={img} alt="로고대체" />
-            </S.Logo>
-          </S.Item>
-          <S.Item>
-            <Link to="/">
-              레퍼런스
-              {Boolean(
-                location.pathname.includes("/ref") || location.pathname === "/"
-              ) && <S.HoverlineUnder1 />}
-            </Link>
-          </S.Item>
-          <S.Item>
-            <Link to="/manage/list">
-              작업물 관리
-              {Boolean(location.pathname.includes("/manage")) && (
-                <S.HoverlineUnder2 />
-              )}
-            </Link>
-          </S.Item>
-          <S.Item>
-            <Link to="/mypage/profile">
-              마이페이지
-              {Boolean(location.pathname.includes("/mypage")) && (
-                <S.HoverlineUnder3 />
-              )}
-            </Link>
-          </S.Item>
+          {CATEGORY_LINKS.map((item) => (
+            <S.Item key={item.name}>
+              <S.StyledLink to={item.link}>{item.name}</S.StyledLink>
+            </S.Item>
+          ))}
         </S.Items>
-      </S.Contents>
-      <S.Contents>
+
+        {/* 로그인 / 로그아웃 시 변경되는 메뉴 */}
         <LoginCheck />
-      </S.Contents>
+      </S.HeaderInner>
     </S.Header>
   );
 }
