c48777d81228fc4a7518bb29b29e6e9954a8d71f
geniee1220 <geniee1220@gmail.com>
2024-01-04 23:00:03 +0900

    Refactor : 인덱스 체크 함수 리팩토링

--- FE/src/page/reference/RefPage/index.jsx
+++ FE/src/page/reference/RefPage/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import Layout from '../../../layout/Layout';
 import Cateogry from '../../../components/common/Category';
@@ -6,53 +7,43 @@ import RefSearch from '../../../containers/reference/RefSearchBar';
 import RefListWrapper from '../../../containers/reference/RefListWrapper';
 import FirstModal from '../../../containers/modal/FirstModal';
 
+import { pageLinks } from '../../../containers/reference/constants';
+
 function RefPage() {
+  const { pathname } = useLocation();
+
   const [searchKeyword, setSearchKeyword] = useState('');
+  const [checkIdx, setCheckIdx] = useState();
   const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
-    //console.log(typeof sessionStorage.getItem('new'));
     if (sessionStorage.getItem('new') === 'true') {
       setModalOpen(true);
-    } else {
     }
   }, []);
+
+  useEffect(() => {
+    Object.keys(pageLinks).forEach((key) => {
+      if (pageLinks[key].path === pathname) {
+        setCheckIdx(key);
+      }
+    });
+  }, [checkIdx]);
 
   const handleSearch = (keyword) => {
     setSearchKeyword(keyword);
   };
 
-  // 디버깅용 코드
-  // useEffect(() => {
-  //   console.log('변경된 검색어', searchKeyword);
-  // }, [searchKeyword]);
-
-  const [checkIdx, setCheckIdx] = useState();
-
   const onChangeCategory = (category) => {
-    if (category === 'all') setCheckIdx(0);
-    if (category === 'idea') setCheckIdx(1);
-    if (category === 'marketing') setCheckIdx(2);
-    if (category === 'video') setCheckIdx(3);
-    if (category === 'design') setCheckIdx(4);
-    if (category === 'it') setCheckIdx(5);
-    if (category === 'etc') setCheckIdx(6);
+    Object.keys(pageLinks).forEach((key) => {
+      if (pageLinks[key].keyword === category) {
+        setCheckIdx(key);
+      }
+    });
   };
-
-  let Lo = window.location.href;
-  useEffect(() => {
-    if (Lo.includes('idea')) setCheckIdx(1);
-    else if (Lo.includes('marketing')) setCheckIdx(2);
-    else if (Lo.includes('video')) setCheckIdx(3);
-    else if (Lo.includes('design')) setCheckIdx(4);
-    else if (Lo.includes('it')) setCheckIdx(5);
-    else if (Lo.includes('etc')) setCheckIdx(6);
-    else setCheckIdx(0);
-  }, [checkIdx]);
 
   return (
     <Layout>
-      {/* TODO : Layout styled-component로 변경 후 props 추가 설정 */}
       <div style={{ flexDirection: 'column' }}>
         {/* 검색창 */}
         <RefSearch onSearch={handleSearch} />
