cb2cf65ddf90b3691c0936df1c561939c18b6472
geniee1220 <geniee1220@gmail.com>
2024-01-07 23:49:25 +0900

    Design : Header 컴포넌트를 fixed에서 sticky로 지정

--- FE/src/layout/Header/Header.styles.js
+++ FE/src/layout/Header/Header.styles.js
@@ -1,11 +1,12 @@
-import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Header = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  position: fixed;
+  position: sticky;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 80px;
   background-color: #fada5e;
