7916d4d588c5cd8298754b9a2a1c1d812df56b21
geniee1220 <geniee1220@gmail.com>
2024-01-08 11:32:11 +0900

    Design : 로그인 페이지 가운데 정렬 및 배경 추가

--- FE/src/layout/Layout.jsx
+++ FE/src/layout/Layout.jsx
@@ -1,12 +1,20 @@
 import Header from './Header';
 import Footer from './Footer/Footer';
 import styles from './Layout.module.css';
+import { useLocation } from 'react-router-dom';
 
 function Layout(props) {
+  const { pathname } = useLocation();
+
   return (
     <>
       <Header />
-      <main className={styles.main}>
+      <main
+        className={styles.main}
+        style={{
+          backgroundColor: pathname === '/sociallogin' ? '#f5f5f5' : 'white',
+        }}
+      >
         <div className={styles.content}>{props.children}</div>
       </main>
       <Footer />
