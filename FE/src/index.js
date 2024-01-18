4a8ac9be535eb152a1aa0e1521fc390b887515d3
geniee1220 <geniee1220@gmail.com>
2024-01-18 21:19:48 +0900

    Refactor : 메타 태그를 공통으로 적용하기 위해 라우터 파일을 App.js로 이동

--- FE/src/index.js
+++ FE/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -14,29 +14,10 @@ import { ThemeProvider } from 'styled-components';
 
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { HelmetProvider } from 'react-helmet-async';
 
-// 페이지
-import SocialLoginPage from './page/sociallogin/SocialLoginPage';
-import UnknownPage from './page/UnknownPage';
-
-import RefPage from './page/reference/RefPage';
-
-import ManageList from './page/manage/ManageList';
-import ManageShare from './page/manage/ManageShare';
-import ManageFeedback from './page/manage/ManageFeedback';
-
-import MyPage from './page/mypage/MyPage';
-import MyPageFAQNew from './containers/mypage/MyPageFAQNew';
-import MyPageFAQDetail from './containers/mypage/MyPageFAQDetail';
-
-import SignUpPage from './page/SignUpPage';
-import KakaoLogin from './containers/sociallogin/KakaoLogin/index.jsx';
-
+// 스타일
 import theme from './layout/theme';
-
-import AuthLayout from './layout/AuthLayout';
-
-import ScrollToTop from './components/common/ScrollToTop/index.jsx';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -55,52 +36,10 @@ root.render(
       <QueryClientProvider client={queryClient}>
         <RecoilRoot>
           <BrowserRouter>
-            <ScrollToTop />
-            <Routes>
-              <Route path="/login" element={<SocialLoginPage />} />
-              <Route path="/signup" element={<SignUpPage />} />
-              <Route path="/sociallogin" element={<SocialLoginPage />} />
-              <Route path="/login/kakao" element={<KakaoLogin />} />
-
-              {/* 레퍼런스 페이지 */}
-              <Route path="/" element={<RefPage />}>
-                {/*<Route path=":id" element={<RefModal />} />*/}
-              </Route>
-              <Route path="/ref/:category" element={<RefPage />}>
-                {/*<Route path=":id" element={<RefModal />} />*/}
-              </Route>
-              <Route path="/ref/search/:search" element={<RefPage />} />
-
-              <Route
-                path="/user/list/:id"
-                element={<ManageList state={false} />}
-              />
-              {/* 다른 사람의 작업물 목록도 보여야 함*/}
-
-              <Route element={<AuthLayout />}>
-                <Route
-                  path="/manage/list"
-                  element={<ManageList state={true} />}
-                />
-                <Route path="/manage/share" element={<ManageShare />} />
-                <Route path="/manage/put/:id" element={<ManageShare />} />
-                <Route path="/manage/feedback" element={<ManageFeedback />} />
-
-                {/* 레퍼런스 수정 */}
-
-                {/* 마이페이지 */}
-                <Route path="/mypage/:id" element={<MyPage />}>
-                  <Route path=":category/new" element={<MyPageFAQNew />} />
-                  <Route
-                    path=":category/:detailId"
-                    element={<MyPageFAQDetail />}
-                  />
-                </Route>
-              </Route>
-              <Route path="*" element={<UnknownPage />} />
-            </Routes>
+            <HelmetProvider>
+              <App />
+            </HelmetProvider>
           </BrowserRouter>
-          <App />
         </RecoilRoot>
       </QueryClientProvider>
     </CookiesProvider>
