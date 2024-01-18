4f70ae794f509c848b7a385a2dea1a1a0a943376
geniee1220 <geniee1220@gmail.com>
2024-01-18 21:21:08 +0900

    Feat : Meta 컴포넌트 생성

---
+++ FE/src/components/common/Meta/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+function Meta({ title = 'Remoa', imageURL = 'https://example.com/image.jpg' }) {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="localhost:3000" />
+      <meta property="og:title" content={title} />
+      <meta property="og:image" content={imageURL} />
+      <meta
+        property="og:description"
+        content="공모전 참여작품들을 탐색하고, 공유하고, 피드백을 받을 수 있어요. 첫 공모전부터 역량 상승까지 레모아에서 시작하세요."
+      />
+      <meta property="og:site_name" content="Remoa" />
+      <meta property="og:locale" content="ko_KR" />
+    </Helmet>
+  );
+}
+
+export default Meta;
