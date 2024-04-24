07720cdc79190ebfc67fb3237ccc318d39c5a80e
Nam Min Ki <nam960216@gmail.com>
2024-04-24 14:48:57 +0900

    fix: 카카오 로그인 관련 수정

--- FE/src/hooks/useAuth.js
+++ FE/src/hooks/useAuth.js
@@ -1,31 +1,42 @@
-import axios from 'axios';
-import { useRecoilState } from 'recoil';
-import { loginState } from '../state/loginState';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { useRecoilState } from "recoil";
+import { loginState } from "../state/loginState";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const token = sessionStorage.getItem("token");
 
 export const useAuth = () => {
   const navigate = useNavigate();
   const [isLogin, setIsLogin] = useRecoilState(loginState);
 
   useEffect(() => {
-    const nickname = sessionStorage.getItem('nickname');
+    const nickname = sessionStorage.getItem("nickname");
 
     nickname ? setIsLogin(true) : setIsLogin(false);
   }, [setIsLogin]);
 
   const handleLogout = async () => {
     try {
-      await axios.post('/BE/user/logout', {}, { withCredentials: true });
+      await axios.put(
+        "/BE/api/member/logout",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
-      sessionStorage.removeItem('nickname');
-      sessionStorage.removeItem('email');
-      sessionStorage.removeItem('new');
+      sessionStorage.removeItem("nickname");
+      sessionStorage.removeItem("email");
+      sessionStorage.removeItem("new");
+      sessionStorage.removeItem("token");
 
       setIsLogin(false);
 
-      alert('로그아웃 되었습니다.');
-      navigate('/');
+      alert("로그아웃 되었습니다.");
+      navigate("/");
     } catch (err) {
       console.log(err);
     }
--- FE/src/setupProxy.js
+++ FE/src/setupProxy.js
@@ -1,12 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-
-  
   app.use(
     "/BE",
     createProxyMiddleware({
-      target: "http://54.180.159.30:8080",
+      // target: "http://54.180.159.30:8080",
+      target: "http://52.78.43.73:8080",
       changeOrigin: true,
       pathRewrite: {
         "^/BE": "", // URL ^/BE -> 공백 변경
