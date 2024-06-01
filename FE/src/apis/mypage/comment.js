d674838aca9fa582009f354dfd1c0acf65b4f75f
Nam Min Ki <nam960216@gmail.com>
2024-06-01 12:19:32 +0900

    fix: 401 관련 에러 수정

--- FE/src/apis/mypage/comment.js
+++ FE/src/apis/mypage/comment.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const token = sessionStorage.getItem("token");
+
 export const getOneComment = async () => {
-  const response = await axios.get(`/BE/user/comment?page=${1}`);
+  const response = await axios.get(`/BE/user/comment?page=${1}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   const {
     data: {
       data: { contents },
@@ -11,7 +17,11 @@ export const getOneComment = async () => {
 };
 
 export const getComment = async (pageNum) => {
-  const response = await axios.get(`/BE/user/comment?page=${pageNum}`);
+  const response = await axios.get(`/BE/user/comment?page=${pageNum}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   const {
     data: {
       data: { contents, totalPages },
--- FE/src/apis/mypage/follow.js
+++ FE/src/apis/mypage/follow.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const token = sessionStorage.getItem("token");
+
 export const getFollow = async (type) => {
-  const response = await axios.get(`/BE/${type}`);
+  const response = await axios.get(`/BE/${type}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   const {
     data: {
       data,
@@ -12,5 +18,9 @@ export const getFollow = async (type) => {
 };
 
 export const postFollow = async (memberId) => {
-  return await axios.post(`/BE/follow/${memberId}`);
+  return await axios.post(`/BE/follow/${memberId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
